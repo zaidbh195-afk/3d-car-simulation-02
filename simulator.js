74bb3097e5cfdbb43275792fe6d4a8dc0f8cbbfc
zaidbh195-afk <zaidbh195@gmail.com>
2026-08-30 18:43:17 +0530

    Initialize Three.js scene and ground plane
    Set up basic Three.js scene with ground plane.

--- simulator.js
+++ simulator.js
@@ -1,1 +1,34 @@
+import * as THREE from
+"https://cdn.jsdelivr.net/npm/three@0.179.1/build/three.module.js";
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+const renderer = new THREE.WebGLRenderer();
 
+renderer.setSize(
+    window.innerWidth,
+    window.innerHeight
+);
+
+document.body.appendChild(renderer.domElement);
+camera.position.set(0, 3, 8);
+const groundGeometry = new THREE.PlaneGeometry(100, 100);
+
+const groundMaterial = new THREE.MeshBasicMaterial({
+    color: 0x444444,
+    side: THREE.DoubleSide
+});
+
+const ground = new THREE.Mesh(
+    groundGeometry,
+    groundMaterial
+);
+
+scene.add(ground);
+
+ground.rotation.x = -Math.PI / 2;
+ground.rotation.x = -Math.PI / 2;
